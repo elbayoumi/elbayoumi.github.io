2f21b58aba443c2cd22cacbce11a975ae9be6fe3
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2025-08-14 15:53:00 +0300

    Added service worker registration and updated PWA configuration

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,3 +263,7 @@
 
 })()
 /////////////////////
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => console.log('Service Worker Registered'));
+}
